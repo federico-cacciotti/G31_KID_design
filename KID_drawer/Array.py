c9ab17b0716ea52a9304b28a9547d1a61a4b52b8
abissoblu <fe.cacciotti@gmail.com>
2022-02-19 00:37:47 +0100

    First commit

---
+++ KID_drawer/Array.py
@@ -0,0 +1,19 @@
+# KID drawer (DXF file generator) - Federico Cacciotti (c)2022
+
+# importa packages
+import ezdxf
+from ezdxf.addons import Importer
+import numpy as np
+
+class Array():
+    '''
+	Parameters:
+		n_pixels: int, number of pixel of the array
+        positions: list of floats, ordered list of (x,y) tuple positions of each pixel in microns
+        rotations: list of floats, ordered list of rotation angle of each pixel in degrees
+	See other function help for more info
+	'''
+    def __init__(self, n_pixels, positions, rotations):
+        self.n_pixels = n_pixels
+        self.positions = positions
+        self.rotations = rotations
---
+++ KID_drawer/__init__.py
@@ -0,0 +1,5 @@
+from . Pixel import *
+from . Array import *
+
+__version__ = '0.1.1'
+__author__ = 'Federico Cacciotti'
