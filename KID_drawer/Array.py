Added rotation, mirroring, and fixed text transformation

KID_drawer/Array.py
@@ -1,20 +1,59 @@
 # KID drawer (DXF file generator) - Federico Cacciotti (c)2022
 
-# importa packages
+# import packages
 import ezdxf
 from ezdxf.addons import Importer
 import numpy as np
+from pathlib import Path
+from os.path import exists
 
 class Array():
     '''
 	Parameters:
-		n_pixels: int, number of pixel of the array
-        positions: list of floats, ordered list of (x,y) tuple positions of each pixel in microns
+        input_dxf_path: string, path to the dxf pixel files
+        n_pixels: int, number of pixel of the array
+        positions: list of floats tuple, ordered list of (x,y) tuple positions of each pixel in microns
         rotations: list of floats, ordered list of rotation angle of each pixel in degrees
+        mirror: list of chars, ordered list of chars of mirroring parameters, ex. 'x' means mirror with respect to the x axis
 	See other function help for more info
 	'''
-    def __init__(self, input_dxf_path, n_pixels, positions, rotations):
+    def __init__(self, input_dxf_path, n_pixels, position, rotation, mirror):
         self.input_dxf_path = input_dxf_path
         self.n_pixels = n_pixels
-        self.positions = positions
-        self.rotations = rotations
+        self.position = position
+        self.rotation = rotation
+        self.mirror = mirror
+
+        # check if files exist
+        for i in range(self.n_pixels):
+            file = self.input_dxf_path / 'pixel_{:d}.dxf'.format(i+1)
+            if not exists(file):
+                print("Error. '"+str(file)+"' does not exists.")
+                return None
+
+        # create the array dxf file
+        array_dxf = ezdxf.new('R2018', setup=True)
+        for i in range(self.n_pixels):
+            # read pixel dxf files
+            pixel_dxf = ezdxf.readfile(Path(self.input_dxf_path, 'pixel_{:d}.dxf'.format(i+1)))
+            for entity in pixel_dxf.modelspace():
+                # the textual index should be translated only
+                # type(entity) == ezdxf.entities.text.Text return True if the
+                # entity is the textual index
+                if not type(entity) == ezdxf.entities.text.Text:
+                    # mirroring
+                    if self.mirror[i] == 'x':
+                        entity.transform(ezdxf.math.Matrix44.scale(sx=-1, sy=1, sz=1))
+                    if self.mirror[i] == 'y':
+                        entity.transform(ezdxf.math.Matrix44.scale(sx=1, sy=-1, sz=1))
+                    # rotation
+                    if self.rotation[i] != 0.0:
+                        entity.transform(ezdxf.math.Matrix44.z_rotate(np.radians(self.rotation[i])))
+                # translation
+                entity.transform(ezdxf.math.Matrix44.translate(self.position[i][0], self.position[i][1], 0.0))
+            importer = Importer(pixel_dxf, array_dxf)
+            importer.import_modelspace()
+            importer.finalize()
+
+        # save array dxf file
+        array_dxf.saveas(self.input_dxf_path.parent / 'array.dxf')

generate_array.py
@@ -1,7 +1,8 @@
 import ezdxf
-from ezdxf.addons import Importer
+#from ezdxf.addons import Importer
 import numpy as np
 from pathlib import Path
+from KID_drawer import Array
 
 N_PIXEL = 415
 PIXEL_PIXEL_DISTANCE = 4200.0 # microns
@@ -9,6 +10,7 @@ NUMBER_PER_ROW = [5, 9, 12, 15, 16, 18, 19, 20, 21, 21, 21, 21, 21, 21, 21, 20, 
 NUMBER_OF_ROWS = len(NUMBER_PER_ROW)
 FIRST_ELEMENT_X_HEX = [4, 1, -1, -3, -4, -5, -6, -7, -8, -8, -9, -9, -10, -10, -11, -11, -12, -12, -12, -12, -12, -11, -11, -8, -8]
 FIRST_ELEMENT_Y_HEX = [-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+MIRRORED_PIXELS = [373, 327, 368, 383, 396, 382, 407, 412, 387, 398, 333]
 
 # nearest 3 disposition
 PIXEL_ORDER = [406, 400, 394, 388, 382, 378, 372, 366, 360, 354, 348, 342, 336, 330, 335, 329, 324, 318, 312, 306,
@@ -38,18 +40,31 @@ pixel_index = 0
 for row,(npr, fst_x, fst_y) in enumerate(zip(NUMBER_PER_ROW, FIRST_ELEMENT_X_HEX, FIRST_ELEMENT_Y_HEX)):
     x = PIXEL_PIXEL_DISTANCE * (fst_x + 0.5*fst_y)
     y = fst_y * PIXEL_PIXEL_DISTANCE * 0.5*np.sqrt(3.0)
+    # rotation
     if(row%2 == 0):
-        rotation = 180
+        rotation = 180.
     else:
-        rotation = 0
+        rotation = 0.
     for npr_i in range(npr):
-        data.append([PIXEL_ORDER[pixel_index], x, y, rotation])
+        # mirroring
+        if PIXEL_ORDER[pixel_index] in MIRRORED_PIXELS:
+            mirror = 'x'
+        else:
+            mirror = 'None'
+        data.append((PIXEL_ORDER[pixel_index], x, y, rotation, mirror))
         x += PIXEL_PIXEL_DISTANCE
         pixel_index += 1
-data = np.array(data)
-# sort array data wrt the pixel index
-data_sorted = data[data[:, 0].argsort()]
 
+data.sort(key=lambda x: x[0])
+
+xy = [tple[1:3] for tple in data]
+r = [tple[3] for tple in data]
+m = [tple[4] for tple in data]
+
+input_path = Path('examples/415/pixels')
+array = Array(input_path, 415, xy, r, m)
+
+'''
 array_dxf = ezdxf.new('R2018')
 
 for [i, x, y, r] in data_sorted:
@@ -63,3 +78,4 @@ for [i, x, y, r] in data_sorted:
     importer.finalize()
 
 array_dxf.saveas('array.dxf')
+'''